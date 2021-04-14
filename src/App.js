e9139e61eba4b4a5b68fc50611e7769f9337b89a
mamathaguntu <mamathaguntu@gmail.com>
2021-04-13 19:31:54 -0700

    modal changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Components/Home'
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Home />
     </div>
   );
 }
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,16 +2,34 @@ import React, { useState, useEffect } from 'react'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Typography from '@material-ui/core/Typography';
+
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'center',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Home = () => {
-  // state variable to store user posts
+  const classes = useStyles();
   const [posts, setPosts] = React.useState([])
-  const [userData, setUserData] = React.useState([])
   const [authorName, setAuthorName] = React.useState('');
   const [catchPhrase, setCatchPhrase] = React.useState('');
   const [body, setBody] = React.useState('');
   const [title, setTitle] = React.useState('');
-
+  const [openModal, setOpenModal] = React.useState(false);
+  // console.log("authorName", authorName);
+  // console.log("catchPhrase", catchPhrase)
   useEffect(() => {
     loadPosts();
   }, []);
@@ -22,37 +40,62 @@ const Home = () => {
       .then(result => setPosts(result));
     // console.log(posts);
   }
-  const getUserData = async (userId) => {
-    const apiUrl = 'https://jsonplaceholder.typicode.com/posts?userId=' + userId;
-    await fetch(apiUrl).then(response => response.json())
-      .then(result => setUserData(result));
+
+  const handleClick = async (userId, postTitle, postBody) => {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users?id=' + userId;
+    const response = await fetch(apiUrl).then((response) => response.json());
+    // const result = await response.json();
+    console.log(response[0].company.name)
+    // setUserData(result);
+
+    setAuthorName(response[0].company.name);
+    setCatchPhrase(response[0].company.catchPhrase);
+    setTitle(postTitle);
+    setBody(postBody);
+    setOpenModal(true);
+  }
+  const handleClose = () => {
+    setOpenModal(false);
   }
 
-  const handleClick = async (userId) => {
-    const apiUrl = 'https://jsonplaceholder.typicode.com/posts?userId=' + userId;
-    await fetch(apiUrl).then(response => response.json())
-      .then(result => setUserData(result));
-  }
   return (
     <div>
       {
-
         posts.map(userPosts =>
-
           <ListItem
             button
-            // selected={selectedIndex === 3}
             key={userPosts.id}
-            onClick={(event) => handleClick(userPosts.userId)}
+            onClick={(event) => handleClick(userPosts.userId, userPosts.title, userPosts.body)}
           >
             <ListItemText primary={userPosts.title} />
           </ListItem>
         )
 
-
       }
-    </div >
+      <Modal
+        open={openModal}
+        onClose={handleClose}
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'
+      >
+        <div className={classes.paper}>
+          <Typography>
+            <h4>Title : {title}</h4>
+          </Typography>
+          <Typography>
+            <h5>Author : {authorName}</h5>
+          </Typography>
+          <Typography>
+            <h5>CatchPhrase : {catchPhrase}</h5>
+          </Typography>
+          <Typography>
+            {body}
+          </Typography>
+          <Button type='submit' variant='contained' color='primary' onClick={handleClose}>Close</Button>
+        </div>
 
+      </Modal>
+    </div>
   )
 }
 
